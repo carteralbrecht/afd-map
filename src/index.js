a2b814d2832dff153bbd54c28e1567aab14d9baf
Carter Albrecht <caalbt@amazon.com>
2022-08-24 14:15:55 -0400

    cesium configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Hello world!');
+// The URL on your server where CesiumJS's static files are hosted.
+window.CESIUM_BASE_URL = 'dist/';
+
+import * as Cesium from 'cesium';
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
+Cesium.Ion.defaultAccessToken = process.env.CESIUM_TOKEN;
+
+// Initialize the Cesium Viewer in the HTML element with the "cesiumContainer" ID.
+const viewer = new Cesium.Viewer('cesiumContainer', {
+  terrainProvider: Cesium.createWorldTerrain()
+});    
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());   
+// Fly the camera to San Francisco at the given longitude, latitude, and height.
+viewer.camera.flyTo({
+  destination : Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+  orientation : {
+    heading : Cesium.Math.toRadians(0.0),
+    pitch : Cesium.Math.toRadians(-15.0),
+  }
+});
